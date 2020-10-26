9a1f77002496bb142ef12a53292c6ed1a21b0cbe
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-10-26 14:45:27 +0900

    Revert "깃새로팜"
    This reverts commit 2d83b954630c183e3794caac1f0faee11d8508e5.

--- Assets/02.Scripts/BodyTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BodyTest : MonoBehaviour
-{
-    public Transform connectToLeg;
-    public Transform connectToWeapon;
-}
--- Assets/02.Scripts/LegTest.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LegTest : MonoBehaviour
-{
-    public Transform connectTobody;
-}
